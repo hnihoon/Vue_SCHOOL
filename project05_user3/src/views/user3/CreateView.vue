d37ec9ff78f5641a2fc62dbfeefd32606595d394
hnihoon <kdh462601@gmail.com>
2023-12-04 18:34:42 +0900

    90일차 수업

--- project05_user3/src/views/user3/CreateView.vue
+++ project05_user3/src/views/user3/CreateView.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>사용자 추가 (CreateView)</h1>
         <p>
-            <router to="/user3/list" class="btn btn-primary">사용자 목록</router>
+            <router-link to="/user3/list" class="btn btn-primary">사용자 목록</router-link>
         </p>
         <div class="container">
           <form>
@@ -12,7 +12,7 @@
                     <label for="name">이름:</label>
                 </th>
                 <td>
-                    <input type="text" id="name" name="name" placeholder="이름 입력하세요" class="form-control" autocomplete="off">
+                    <input type="text" id="name" name="name" placeholder="이름 입력하세요" class="form-control" autocomplete="off" v-model="result.name" :class="{active:active.name}">
                 </td>
             </tr>
             <tr>
@@ -20,7 +20,7 @@
                     <label for="email">이메일:</label>
                 </th>
                 <td>
-                    <input type="email" id="email" name="email" placeholder="이메일 입력하세요" class="form-control" autocomplete="off">
+                    <input type="email" id="email" name="email" placeholder="이메일 입력하세요" class="form-control" autocomplete="off" v-model="result.email" :class="{active:active.email}">
                 </td>
             </tr>
             <tr>
@@ -28,7 +28,7 @@
                     <label for="pwd">비밀번호:</label>
                 </th>
                 <td>
-                    <input type="password" id="pwd" name="pwd" placeholder="비밀번호 입력하세요" class="form-control" autocomplete="off">
+                    <input type="password" id="pwd" name="pwd" placeholder="비밀번호 입력하세요" class="form-control" autocomplete="off" v-model="result.pwd" :class="{active:active.pwd}">
                 </td>
             </tr>
             <tr>
@@ -36,10 +36,10 @@
                     성별:
                 </th>
                 <td>
-                    <input type="radio" id="radio1" name="gender" value="1" checked>
+                    <input type="radio" id="radio1" name="gender" value="1" v-model="result.gender">
                     <label for="radio1">남성</label>
                     &nbsp;&nbsp;&nbsp;
-                    <input type="radio" id="radio2" name="gender" value="0" checked>
+                    <input type="radio" id="radio2" name="gender" value="0" v-model="result.gender">
                     <label for="radio2">여성</label>
                 </td>
             </tr>
@@ -47,23 +47,62 @@
           </form>
         </div>
         <div>
-          <button class="btn btn-warning">생성</button>
+          <button class="btn btn-warning" @click="save">생성</button>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   data () {
     return {
+      result: { name: '', email: '', pwd: '', gender: '' },
+      active: { name: false, email: false, pwd: false }
     }
   },
   methods: {
+    save () {
+      if (this.result.name === '') {
+        alert('이름을 입력해 주세요')
+        this.active.name = true
+        return
+      } else {
+        this.active.name = false
+      }
+
+      if (this.result.email === '') {
+        alert('이메일을 입력해 주세요')
+        this.active.email = true
+        return
+      } else {
+        this.active.email = false
+      }
+
+      if (this.result.pwd === '') {
+        alert('비밀번호를 입력해 주세요')
+        this.active.pwd = true
+        return
+      } else {
+        this.active.pwd = false
+      }
+
+      console.log(this.result)
+      axios
+        .put('http://localhost:9095/user3/save', this.result)
+        .then((res) => {
+          console.log(res)
+          alert('사용자가 추가되었습니다.')
+          this.$router.push({ name: 'ListView' })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
-
 <style>
   @import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
 </style>
--- project05_user3/src/views/user3/ListView.vue
+++ project05_user3/src/views/user3/ListView.vue
@@ -6,16 +6,39 @@
             &nbsp;
             <router-link to="/user3/create" class="btn btn-success">사용자 추가</router-link>
         </p>
+        <div class="container">
+          <table class="table table-hover">
+            <thead>
+              <tr class="info">
+                <th>이름</th>
+                <th>이메일</th>
+                <th>가입날짜</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="row in result" :key="row.no">
+                <td>{{ row.name }}</td>
+                <td>{{ row.email }}</td>
+                <td>{{ row.regdate }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
     </div>
 </template>
 
 <script>
-// 주의사항) axios모듈 임포트하고 사용하지 않으면 에러 발생 'axios is defined but never used
+// 주의사항) axios모듈 임포트하고 사용하지 않으면 에러 발생 -> 무시해도 됨
+// 'axios is defined but never used
 import axios from 'axios'
 export default {
   data () {
     return {
+      result: []
     }
+  },
+  created () {
+    this.getList()
   },
   methods: {
     getData () {
@@ -27,6 +50,16 @@ export default {
         .catch((error) => {
           console.log(error)
         })
+    },
+    getList () {
+      axios
+        .post('http://localhost:9095/user3/list')
+        .then((response) => {
+          this.result = response.data.result
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
   }
 }
@@ -34,4 +67,7 @@ export default {
 
 <style>
   @import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
+  .info th {
+    text-align: center;
+  }
 </style>
